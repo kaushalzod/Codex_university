170721eaaa4819fb8da7c382fa02b7c14dc1bd30
kaushalzod <kaushaljhod@gmail.com>
2021-09-08 18:12:38 +0530

    About Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {anchorScrolling: 'enabled',scrollPositionRestoration: 'enabled'})],
+  imports: [RouterModule.forRoot(routes, 
+    {
+      anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
